a3ce03fa370bacb51f128ba8c8b6fc67553bcefc
John Emrick <emrickj248@comcast.net>
2019-03-24 16:29:40 -0400

    Add files via upload
    Bootstrap 3 themes now use the latest version to fix XSS issue.

--- theme1af.php
+++ theme1af.php
@@ -25,7 +25,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title><?php echo strip_tags($xml->title) ?></title>
 <style>
@@ -152,6 +152,6 @@ t1 { white-space: pre-wrap;}
 	  <center>This website was created using <a href="https://www.gem-editor.com">GEM</a>.</center>
    </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </body>
 </html>
